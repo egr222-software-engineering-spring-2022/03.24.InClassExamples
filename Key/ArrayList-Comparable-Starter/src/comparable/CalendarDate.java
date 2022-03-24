44df9162c55ef9a4df7eed875345325eccef4ef4
Larry Clement <lclement@calbaptist.edu>
2022-03-23 18:09:05 -0700

    Update comparable starter files

--- Key/ArrayList-Comparable-Starter/src/comparable/CalendarDate.java
+++ Key/ArrayList-Comparable-Starter/src/comparable/CalendarDate.java
@@ -1,11 +1,11 @@
 package comparable;
 
 //Make this code implement Comparable interface
-public class CalendarDate implements Comparable<CalendarDate> {
+public class CalendarDate {
     private int year;
     private int month;
     private int day;
-    
+
     public CalendarDate(int year, int month, int day) {
         this.year = year;
         this.month = month;
@@ -15,30 +15,19 @@ public class CalendarDate implements Comparable<CalendarDate> {
     public int getYear() {
         return year;
     }
-    
+
     public int getMonth() {
         return month;
     }
-    
+
     public int getDay() {
         return day;
     }
-    
+
     public String toString() {
         return year + "/" + String.format("%02d", month) + "/" + String.format("%02d", day);
     }
-    
+
 // YOUR CODE GOES HERE
-    @Override
-    public int compareTo(CalendarDate other) {
-        if (this.year == other.year) {
-            // years are the same
-            if (this.month == other.month) {
-                // year and month are the same
-                return this.day - other.day;
-            }
-            return this.month - other.month;
-        }
-        return this.year - other.year;
-    }
+
 }
